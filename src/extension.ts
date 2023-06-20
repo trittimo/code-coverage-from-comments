7fa768cbb6485fc9d0c9a6f06b67b577ec9cf1f5
Michael Trittin <michaeltrittin@gmail.com>
2023-06-20 19:33:55 -0400

    WIP

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,93 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import { readFileSync } from 'fs';
 import * as vscode from 'vscode';
+
+var highlightProviders: vscode.Disposable[] = [];
+
+class HighlightProvider implements vscode.DocumentHighlightProvider {
+	provideDocumentHighlights(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): vscode.ProviderResult<vscode.DocumentHighlight[]> {
+		console.log("Document requesting highlights");
+		let range = new vscode.Range(0, 0, document.lineCount, 0);
+		let result: vscode.DocumentHighlight[] = [];
+		if (document.validateRange(range)) {
+			let highlight: vscode.DocumentHighlight = new vscode.DocumentHighlight(range);
+			result.push(highlight);
+		} else {
+			console.log("Range unnacceptable");
+		}
+		return result;
+	}
+}
+
+function createConfigChangedWatcher(context: vscode.ExtensionContext) {
+	return (uri: vscode.Uri) => {
+		try {
+			let config = JSON.parse(readFileSync(uri.fsPath, "utf8"));
+			if (typeof config !== 'object' || config === null) {
+				throw new Error();
+			}
+
+			highlightProviders.forEach(h => h.dispose());
+			highlightProviders = [];
+
+			let targets: string[] = config?.targets;
+			if (targets !== null && targets.length > 0) {
+				targets.forEach(globPattern => {
+					if (vscode.workspace.workspaceFolders === null || vscode.workspace.workspaceFolders === undefined) {
+						throw new Error("Workspace folders are null or undefined");
+					}
+					let docSelector: vscode.DocumentSelector = {
+						pattern: new vscode.RelativePattern(vscode.workspace.workspaceFolders[0], globPattern)
+					};
+
+					console.log(`Adding a highlight provider for documents matching '${globPattern}'`)
+					
+					let disposable = vscode.languages.registerDocumentHighlightProvider(docSelector, new HighlightProvider());
+					highlightProviders.push(disposable);
+
+					context.subscriptions.push(disposable);
+				})
+			}
+		} catch {
+			console.log("Unable to load extension due to failure parsing config");
+			return;
+		}
+
+		console.log("Successfully loaded config");
+	}
+}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	var configChangedWatcher = createConfigChangedWatcher(context);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "coverage-from-comments" is now active!');
+	if (vscode.workspace.workspaceFolders === null || vscode.workspace.workspaceFolders === undefined) {
+		console.log("Cannot load extension because no folders are open");
+		return;
+	}
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('coverage-from-comments.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from coverage-from-comments!');
+	var watcher = vscode.workspace.createFileSystemWatcher(
+		new vscode.RelativePattern(vscode.workspace.workspaceFolders[0], ".vscode/highlight-provider-config.json"));
+	watcher.onDidChange(configChangedWatcher);
+	watcher.onDidCreate(configChangedWatcher);
+
+	vscode.window.onDidChangeActiveTextEditor(e => {
+		console.log(e?.document.fileName);
+		// let pattern = new vscode.RelativePattern(vscode.workspace.workspaceFolders[0], ".vscode/highlight-provider-config.json");
+		
 	});
 
-	context.subscriptions.push(disposable);
+
+	vscode.workspace.findFiles(".vscode/highlight-provider-config.json").then(uris => {
+		if (uris.length > 0) {
+			configChangedWatcher(uris[0]);
+		}
+	});
+	
+
+	console.log('Congratulations, your extension "coverage-from-comments" is now active!');
 }
 
 // This method is called when your extension is deactivated
